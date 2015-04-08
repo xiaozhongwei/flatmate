4d7758970358f5a2af7b5b57b8715e7671fc1557
LiuXiaoJie <xiaojie7337@gmail.com>
2015-04-08 13:41:05 +0800

    update

--- app/routes/profile/change-password.js
+++ app/routes/profile/change-password.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.createRecord('profile/changePassword');
+    return this.session.get("user");
+  },
+  setupController: function(controller, model) {
+    this._super(controller, model);
+
+    controller.set("passwordModel", this.store.createRecord('profile/changePassword'));
+
+
   }
 });
