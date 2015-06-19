d75e24845097d6def9887531f422a41eafd7d5ba
LiuXiaoJie <xiaojie7337@gmail.com>
2015-06-19 23:13:45 +0800

    update

---
+++ app/transforms/guest-number.js
@@ -0,0 +1,14 @@
+export default DS.Transform.extend({
+  mapping : [
+    /*{ id: 0, text: "" },*/
+    { id: 1, text: "1 guest" },
+    { id: 2, text: "2 guests" },
+    { id: 3, text: "3 guests" },
+    { id: 4, text: "4 guests" },
+    { id: 5, text: "5 guests" },
+    { id: 6, text: "6 guests" },
+    { id: 7, text: "7 guests" },
+    { id: 8, text: "8 guests" },
+    { id: 9, text: "9 guests" }
+  ]
+});
